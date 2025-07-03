5459346ee14bee58c87470686e9a859941e4e368
Badal Kumar Rai <badalrai242@gmail.com>
2025-07-03 12:07:25 +0530

    Final working build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 👈 your GitHub repo name with trailing slash
+  base: '/paradise-nursery-IBM/',
   plugins: [react()],
 });
